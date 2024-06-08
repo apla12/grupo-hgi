4522eb2f3b1019168092f92e35d3e5550c625293
apla12 <anapaulalopes12@gmail.com>
2024-06-08 01:23:06 +0100

    alteração de layout

--- src/app/components/business-units/business-units.component.ts
+++ src/app/components/business-units/business-units.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { BtnDetalheComponent } from '../btn-detalhes/btn-detalhes.component';
 
 @Component({
   selector: 'app-business-units',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, BtnDetalheComponent],
   providers: [],
   templateUrl: './business-units.component.html',
   styleUrls: ['./business-units.component.scss']
