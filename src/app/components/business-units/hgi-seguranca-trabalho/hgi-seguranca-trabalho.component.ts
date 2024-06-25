e783ecbcc596431d77ccfad4f5f27fb5cf1ea0c9
apla12 <anapaulalopes12@gmail.com>
2024-06-25 15:53:03 +0100

    add hgi segurança do trabalho

--- src/app/components/business-units/hgi-seguranca-trabalho/hgi-seguranca-trabalho.component.ts
+++ src/app/components/business-units/hgi-seguranca-trabalho/hgi-seguranca-trabalho.component.ts
@@ -1,20 +1,18 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../footer/footer.component';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-hgi-seguranca-trabalho',
   standalone: true,
-  imports: [NgOptimizedImage, FooterComponent],
+  imports: [NgOptimizedImage, MatListModule, FooterComponent],
   providers: [],
   templateUrl: './hgi-seguranca-trabalho.component.html',
-  styleUrls: ['./hgi-seguranca-trabalho.component.scss']
+  styleUrls: ['./hgi-seguranca-trabalho.component.scss'],
 })
 export class HgiSegurancaTrabalhoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
