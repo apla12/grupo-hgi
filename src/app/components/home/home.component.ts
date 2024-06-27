1fed0280493f88dfc6d5694bd30d983e2b6fd6e7
apla12 <anapaulalopes12@gmail.com>
2024-06-28 00:51:27 +0100

    update contato

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
-import { ContactComponent } from '../contact/contact.component';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { BusinessUnitsComponent } from '../business-units/business-units.component';
@@ -16,7 +15,6 @@ import { ClientComponent } from '../client/client.component';
     AboutComponent,
     BusinessUnitsComponent,
     ClientComponent,
-    ContactComponent,
     FooterComponent,
   ],
   providers: [],
